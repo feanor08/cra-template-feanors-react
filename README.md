64a97b9ba3654c2d005faaae5a0782e928195204
Adarsh ML <mladarsh08@gmail.com>
2020-07-06 18:34:13 +0530

    Add template structure

---
+++ README.md
@@ -0,0 +1,21 @@
+# Welcome to My Custom Template!
+
+## How To Install
+
+### yarn
+
+`yarn create react-app <app-name> --template feanors-react`
+
+### npm
+
+`npm init react-app <app-name> --template feanors-react`
+
+### npx
+
+`npx create-react-app <app-name> --template feanors-react`
+
+### global
+
+> If create-react-app is installed globally on your computer you can use this command:
+
+`create-react -app <app-name> --template feanors-react`
---
+++ template/prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  printWidth: 120, // will ensure that your code does not exceed more then 100 characters
+  singleQuote: true, // will convert all your double quotes into single quotes
+  trailingComma: 'all', // will ensure there is a dangling comma at the end of last object property
+  bracketSpacing: true, // prints spaces between object literals
+  jsxBracketSameLine: false, // will put ">" of a multi line JSX element on the last line
+  tabWidth: 2, // specifies the number of spaces per indentation level.
+  semi: true, // will print ";"" at the end statements.
+};
